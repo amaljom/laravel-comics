9090032e4394634b28a4b7122f1cd45259d0e0fc
amaljom <amaljom99@gmail.com>
2022-09-07 17:29:22 +0200

    footer implementato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <h1>ciaoo</h1>
+    
 @endsection
--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,63 @@
-<div>
-    <h1>ciao sono il footer</h1>
-</div>
+<footer>
+    <!-- inseriamo il background e creiamo all' interno due div che conterrano le liste e il logo -->
+    <div class="container">
+        <!-- LISTE -->
+        <div class="container-ul">
+            <div>
+                <ul>
+                    <li> <h2>DC COMICS</h2> </li>
+                    
+                </ul>
+                <ul>
+                    <li> <h2>SHOP</h2> </li>
+                    
+                </ul>
+            </div>
+            <div class="flex-ul">
+                <ul class="ul-special">
+                    <li> <h2>DC</h2> </li>
+                    
+                </ul>
+                <ul>
+                    <li> <h2>SITES</h2> </li>
+                    
+                </ul>
+            </div>
+        </div>
+        <!-- IMMAGINE -->
+        <div>
+            <img class="bg-logo" src="../assets/img/dc-logo-bg.png" alt="">
+        </div>
+    </div>
+    <div class="index-definer">
+        <section class="container2">
+            <div class="signUpBorder">
+                <a href=""> SIGN-UP NOW!</a>  
+            </div>
+            <div>
+                <ul class="footerUL">
+                    <li>
+                        <h2>FOLLOW US</h2>
+                    </li>
+                    <li>
+                        <img src="../assets/img/footer-facebook.png" alt="">
+                    </li>
+                    <li>
+                        <img src="../assets/img/footer-periscope.png" alt="">
+                    </li>
+                    <li>
+                        <img src="../assets/img/footer-pinterest.png" alt="">
+                    </li>
+                    <li>
+                        <img src="../assets/img/footer-twitter.png" alt="">
+                    </li>
+                    <li>
+                        <img src="../assets/img/footer-youtube.png" alt="">
+                    </li>
+                </ul>
+            </div>
+        </section>
+        
+    </div>
+    <div class="bg-last"></div>
+  </footer>
