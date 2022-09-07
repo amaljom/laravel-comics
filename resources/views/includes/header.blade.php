b11d6453b6f039bb3cf9102e3ec7bc6ce2cf4e40
amaljom <amaljom99@gmail.com>
2022-09-07 16:58:10 +0200

    aggiunto header e il suo css

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,3 +1,23 @@
-<div>
-    <p>ciao sono l'header</p>
-</div>
+
+<header>
+        <nav class="navbar">
+            <!-- logo -->
+            <div>
+                <img src="../assets/img/dc-logo.png" alt="Non trovato">
+            </div>
+            <!-- lista -->
+            <div>
+                <ul>
+                    <li>
+                        ciao
+                    </li>
+                    <li>
+                        un altro
+                    </li>
+                    <li>
+                        un altro
+                    </li>
+                </ul>
+            </div>
+        </nav>
+</header>
