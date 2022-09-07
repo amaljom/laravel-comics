abc4f26511d59baa6c2d009d370c78309b809c12
amaljom <amaljom99@gmail.com>
2022-09-07 17:47:45 +0200

    aggiunto main e corretti i path

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts.main')
 
 @section('main-content')
+<main>
     
+    <!-- QUA INSERIAMO L'IMMAGINE -->
+    <div class="container black-band">  </div>
+    <!-- CREO UN DIV PER CONTENERE TUTTI GLI ELEMENTI CHE DOVRO INSERIRE COME COMPONENT -->
+    
+    <section class="comics-container">
+        <div class="posizionamento-elementi">
+            <div class="button-current-series">
+                <a @click.prevent href="#"> <p>CURRENT SERIES</p> </a>
+            </div>
+           <div class="wrapper-comics">
+                <div class="width-img-wrapper">
+                    <img class="img-wrapper" :src="img" alt="">
+                    <p></p>
+                </div>
+           </div>
+           <div class="button-loader">
+               <a @click.prevent href="#"><p>Load more</p></a> 
+           </div>
+        </div>
+    </section>
+    <!-- div azzurro cche richiamo con un secondo component -->
+    <div class="content-azzurro ">
+            <div class="posizionamento-elementi content-container content-container-images">
+                <ul>
+                    <li>
+                        <img :src="require(`../assets/buy${img}`)" alt="text">
+                        <p class="text-style">{{}}</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+  </main>
 @endsection
