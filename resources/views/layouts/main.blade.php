9870b5c9084235aba3c02d7030be74f68eb25713
amaljom <amaljom99@gmail.com>
2022-09-07 18:39:48 +0200

    correzione css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>comics</title>
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Montserrat:wght@200;300;400;500;600;700&family=Mukta:wght@200;300;400;500&family=Open+Sans:wght@400;500;600;700&family=PT+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
 </head>
 <body>
